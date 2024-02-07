7abb375c1ed228a9fdeeb07e10f839047b0ee539
Andres J. Nazzari <lordcliff@gmail.com>
2024-02-07 14:54:58 -0300

    Add initial project files and configurations

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fs-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass'
+})
+export class AppComponent {
+  title = 'WakuWaku';
+}
