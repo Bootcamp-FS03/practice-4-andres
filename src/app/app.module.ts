0a686f67218cba0b2ba2400e70fae8fe8b419790
Andres Nazzari <andres.nazzari@jala.university>
2024-02-28 14:51:24 -0300

    Add authorization header interceptor provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-import { AppRoutingModule } from './app-routing.module';
+import { addAuthorizationHeaderInterceptorProvider } from './core/providers/add-authorization-header-interceptor.provider';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [HttpClientModule, BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSnackBarModule],
-  providers: [],
+  providers: [addAuthorizationHeaderInterceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
