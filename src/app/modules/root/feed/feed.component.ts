d3a1ec580e682ed6cad7c7077f524385931db206
Andres Nazzari <andres.nazzari@jala.university>
2024-02-28 14:40:37 -0300

    Refactor feed component to display posts dynamically

--- src/app/modules/root/feed/feed.component.ts
+++ src/app/modules/root/feed/feed.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../../../core/models/post.model';
+import { PostService } from '../../../core/services/post/post.service';
 
 @Component({
   selector: 'fs-feed',
   templateUrl: './feed.component.html',
-  styleUrl: './feed.component.sass'
+  styleUrl: './feed.component.sass',
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit {
+  posts$!: Observable<Post[]>;
 
+  constructor(public readonly postService: PostService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.postService.getPosts();
+  }
+
+  postTrackByFn(index: number, post: Post): string {
+    return post._id;
+  }
 }
