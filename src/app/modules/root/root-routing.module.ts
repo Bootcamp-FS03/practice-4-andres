3f17201569a6dec1d141452c74bf9ad409b384c7
Andres Nazzari <andres.nazzari@jala.university>
2024-02-28 14:18:29 -0300

    Add routes for home and feed components

--- src/app/modules/root/root-routing.module.ts
+++ src/app/modules/root/root-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FeedComponent } from './components/feed/feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'feed', pathMatch: 'full' },
+      { path: 'feed', component: FeedComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class RootRoutingModule { }
+export class RootRoutingModule {}
