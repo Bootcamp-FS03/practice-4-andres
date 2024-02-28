8fc175ad8cd260bb65cc817e43122c5a6e01173e
Andres Nazzari <andres.nazzari@jala.university>
2024-02-28 14:18:24 -0300

    Add material modules and components to root module

--- src/app/modules/root/root.module.ts
+++ src/app/modules/root/root.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 import { RootRoutingModule } from './root-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { LayoutComponent } from '../../shared/components/layout/layout.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, RootRoutingModule],
+  declarations: [HomeComponent, FeedComponent, LayoutComponent],
+  imports: [CommonModule, RootRoutingModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule],
 })
 export class RootModule {}
