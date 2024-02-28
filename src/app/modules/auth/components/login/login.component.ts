dec759caf5f287e9919533f193e2ec4ffd5a85c0
Andres Nazzari <andres.nazzari@jala.university>
2024-02-28 13:16:13 -0300

    Refactor login component form and styles

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -11,8 +11,16 @@ export class LoginComponent {
 
   constructor(private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
-      password: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+          Validators.pattern(/^((?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[@#%$£&\.*])\S{4,20})$/),
+        ],
+      ],
     });
   }
 
@@ -23,12 +31,18 @@ export class LoginComponent {
     }
   }
   getErrorMessage(formField: string): string {
-    return this.loginForm.get(formField)?.hasError('required')
-      ? 'You must enter a value'
-      : this.loginForm.get(formField)?.hasError('minlength')
-      ? `The ${formField} must be at least 3 characters`
-      : this.loginForm.get(formField)?.hasError('maxlength')
-      ? `The ${formField} cannot be more than 20 characters`
-      : '';
+    if (this.loginForm?.get(formField)?.hasError('required')) {
+      return `You must enter a value.`;
+    }
+    if (this.loginForm?.get(formField)?.hasError('minlength')) {
+      return `The ${formField} must be at least 4 characters`;
+    }
+    if (this.loginForm?.get(formField)?.hasError('maxlength')) {
+      return `The ${formField} cannot be more than 20 characters`;
+    }
+    if (this.loginForm?.get(formField)?.hasError('pattern')) {
+      return `The ${formField} must have at least one uppercase, one lowercase, one number, and at least one of these characters: @ # % $ £ & \ * `;
+    }
+    return '';
   }
 }
