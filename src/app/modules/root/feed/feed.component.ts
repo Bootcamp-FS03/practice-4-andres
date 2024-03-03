b5cf910df6beaff25750a35e64561d0b072d076b
Andres Nazzari <andres.nazzari@jala.university>
2024-03-03 09:32:08 -0300

    Refactor feed component to display loading spinner and handle empty posts

--- src/app/modules/root/feed/feed.component.ts
+++ src/app/modules/root/feed/feed.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+
+import { Observable, Subscription, combineLatest, map, of, tap } from 'rxjs';
+
 import { Post } from '../../../core/models/post.model';
 import { PostService } from '../../../core/services/post/post.service';
+import { Profile } from '../../../core/models/profile.model';
+import { ProfileService } from '../../../core/services/profile/profile.service';
 
 @Component({
   selector: 'fs-feed',
@@ -9,15 +13,44 @@ import { PostService } from '../../../core/services/post/post.service';
   styleUrl: './feed.component.sass',
 })
 export class FeedComponent implements OnInit {
-  posts$!: Observable<Post[]>;
+  private postsUpdatedSubscription: Subscription | undefined;
 
-  constructor(public readonly postService: PostService) {}
+  posts$!: Observable<Post[]>;
+  profile$!: Observable<Profile>;
+  loading: boolean = true;
+  constructor(public readonly postService: PostService, public readonly profileService: ProfileService) {}
 
   ngOnInit(): void {
-    this.posts$ = this.postService.getPosts();
+    this.loadPosts();
+    this.subscribeToPostsUpdatedEvent();
+  }
+
+  loadPosts() {
+    combineLatest<[Post[], Profile]>([this.postService.getPosts(), this.profileService.getProfile()])
+      .pipe(
+        tap(() => (this.loading = false)),
+        map(([posts]) => posts.sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()))
+      )
+      .subscribe(posts => (this.posts$ = of(posts)));
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeFromPostsUpdatedEvent();
   }
 
   postTrackByFn(index: number, post: Post): string {
     return post._id;
+  }
+
+  subscribeToPostsUpdatedEvent(): void {
+    this.postsUpdatedSubscription = this.postService.postsUpdated().subscribe(() => {
+      this.loadPosts();
+    });
+  }
+
+  unsubscribeFromPostsUpdatedEvent(): void {
+    if (this.postsUpdatedSubscription) {
+      this.postsUpdatedSubscription.unsubscribe();
+    }
   }
 }
