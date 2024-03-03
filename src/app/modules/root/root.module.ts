86b1a3489e18af40bd6253a6efc662c487c38cc0
Andres Nazzari <andres.nazzari@jala.university>
2024-03-03 09:31:51 -0300

    Add Angular Material modules and components to RootModule

--- src/app/modules/root/root.module.ts
+++ src/app/modules/root/root.module.ts
@@ -1,19 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { RootRoutingModule } from './root-routing.module';
+import { LayoutComponent } from '../../shared/components/layout/layout.component';
 import { HomeComponent } from './home/home.component';
 import { FeedComponent } from './feed/feed.component';
-import { LayoutComponent } from '../../shared/components/layout/layout.component';
 import { PostComponent } from './components/post/post.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
 
 @NgModule({
-  declarations: [HomeComponent, FeedComponent, LayoutComponent, PostComponent],
-  imports: [CommonModule, RootRoutingModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule],
+  declarations: [HomeComponent, FeedComponent, LayoutComponent, PostComponent, PostFormComponent, AddPostComponent],
+  imports: [
+    CommonModule,
+    RootRoutingModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+  ],
 })
 export class RootModule {}
