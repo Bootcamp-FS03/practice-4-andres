b5bf8a7bc15899dd5a7824d15742497cc09149f3
Andres Nazzari <andres.nazzari@jala.university>
2024-03-05 13:23:16 -0300

    Refactor feed component to use postService.posts() method

--- src/app/modules/root/feed/feed.component.ts
+++ src/app/modules/root/feed/feed.component.ts
@@ -13,44 +13,18 @@ import { ProfileService } from '../../../core/services/profile/profile.service';
   styleUrl: './feed.component.sass',
 })
 export class FeedComponent implements OnInit {
-  private postsUpdatedSubscription: Subscription | undefined;
-
-  posts$!: Observable<Post[]>;
-  profile$!: Observable<Profile>;
   loading: boolean = true;
+
   constructor(public readonly postService: PostService, public readonly profileService: ProfileService) {}
 
   ngOnInit(): void {
     this.loadPosts();
-    this.subscribeToPostsUpdatedEvent();
   }
 
   loadPosts() {
-    combineLatest<[Post[], Profile]>([this.postService.getPosts(), this.profileService.getProfile()])
-      .pipe(
-        tap(() => (this.loading = false)),
-        map(([posts]) => posts.sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()))
-      )
-      .subscribe(posts => (this.posts$ = of(posts)));
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribeFromPostsUpdatedEvent();
-  }
-
-  postTrackByFn(index: number, post: Post): string {
-    return post._id;
-  }
-
-  subscribeToPostsUpdatedEvent(): void {
-    this.postsUpdatedSubscription = this.postService.postsUpdated().subscribe(() => {
-      this.loadPosts();
-    });
-  }
-
-  unsubscribeFromPostsUpdatedEvent(): void {
-    if (this.postsUpdatedSubscription) {
-      this.postsUpdatedSubscription.unsubscribe();
-    }
+    this.postService
+      .getPosts()
+      .pipe(tap(() => (this.loading = false)))
+      .subscribe();
   }
 }
